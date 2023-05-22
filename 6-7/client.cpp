ad8739ab6f11badb115e483830b9a86c15daf77d
Seleznev Grigory <58052549+Grisha1232@users.noreply.github.com>
2023-05-22 18:03:03 +0300

    Create client.cpp

---
+++ 6-7/client.cpp
@@ -0,0 +1,152 @@
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <time.h>
+#include <unistd.h>
+
+#define SERVER_IP "127.0.0.1"
+#define PORT 12345
+
+void start_dinner(int philosopher_id, int socket, int viewer_socket) {
+  int count_of_meals = 0;
+  char buff[1024];
+  // states = 0 or 1 or 2;
+  // state 0 is for thinking and taking forks after
+  // state 1 is for taking forks and eat;
+  // state 2 is for giving back forks
+  int current_state = 0;
+  while (count_of_meals < 5) {
+    // sending numbers 1, 2 -- refers to server that 1 - reserve forks, 2 - unreserve forks
+    if (current_state == 0) {
+      printf("Phil is thinking\n");
+      sprintf(buff, "Philosopher %d: is thinking\n", philosopher_id);
+      if (send(viewer_socket, buff, sizeof buff, 0) == -1) {
+        perror("Error sending message to viewer");
+        exit(1);
+      }
+      if (send(socket, &philosopher_id, sizeof philosopher_id, 0) == -1) {
+        perror("Error sending reservation");
+        exit(1);
+      }
+      current_state = 1;
+      sleep(rand() % 5 + 1);
+    } else if (current_state == 1) {
+      int result;
+      if (read(socket, &result, sizeof result) == -1) {
+        perror("Error reading of result taking the forks");
+        exit(1);
+      }
+      // philosopher cant take forks at this moment
+      if (result == 0) {
+        current_state = 0;
+      } else {
+        printf("Takes two forks and eating\n");
+        sprintf(buff, "Philosopher %d: Takes two forks and eating\n", philosopher_id);
+        if (send(viewer_socket, buff, sizeof buff, 0) == -1) {
+          perror("Error sending message to viewer");
+          exit(1);
+        }
+        current_state = 2;
+
+        sleep(rand() % 5 + 1);
+      }
+    } else {
+      count_of_meals++;
+      current_state = 0;
+      int negative = -philosopher_id;
+      if (send(socket, &negative, sizeof negative, 0) == -1) {
+        perror("Error sending back forks");
+        exit(1);
+      }
+
+      printf("Giving back two forks\n");
+      sprintf(buff, "Philosopher %d: Giving back two forks\n", philosopher_id);
+      if (send(viewer_socket, buff, sizeof buff, 0) == -1) {
+        perror("Error sending message to viewer");
+        exit(1);
+      }
+      sleep(rand() % 5 + 1);
+    }
+  }
+  int stop = -111;
+  if (send(socket, &stop, sizeof stop, 0) == -1){
+    perror("Error sending stop message");
+    exit(1);
+  }
+
+  sprintf(buff, "stop");
+  if (send(viewer_socket, buff, sizeof buff, 0) == -1) {
+    perror("Error sending message to viewer");
+    exit(1);
+  }
+}
+
+int main() {
+  srand(time(NULL));
+  int client_socket, viewer_socket;
+  int port;
+  char address[1024];
+  struct sockaddr_in server_addr, viewer_addr;
+
+  printf("Enter the address (format *.*.*.*): ");
+  scanf("%[^\n]", address);
+
+  printf("Enter the port: ");
+  scanf("%d", &port);
+
+  // Создание клиентского сокета
+  client_socket = socket(AF_INET, SOCK_STREAM, 0);
+  if (client_socket == -1) {
+    perror("Error creating client socket");
+    exit(1);
+  }
+
+  // Настройка адреса сервера
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_port = htons(port);
+  if (inet_pton(AF_INET, address, &(server_addr.sin_addr)) <= 0) {
+    perror("Error setting server address");
+    exit(1);
+  }
+
+  viewer_socket = socket(AF_INET, SOCK_STREAM, 0);
+  if (viewer_socket == -1) {
+    perror("Error creating viewer socket");
+    exit(1);
+  }
+  viewer_addr.sin_family = AF_INET;
+  viewer_addr.sin_port = htons(8080);
+  viewer_addr.sin_addr.s_addr = INADDR_ANY;
+  if (connect(viewer_socket, (struct sockaddr*) &viewer_addr, sizeof viewer_addr) == -1) {
+    perror("Error connecting viewer socket");
+    exit(1);
+  }
+
+  // Подключение к серверу
+  if (connect(client_socket, (struct sockaddr *) &server_addr, sizeof(server_addr)) == -1) {
+    perror("Error connecting to server");
+    exit(1);
+  }
+
+  int id;
+  printf("Enter the philosopher id: ");
+  scanf("%d", &id);
+
+  if (send(client_socket, &id, sizeof(id), 0) == -1) {
+    perror("Error sending philosopher ID");
+    exit(1);
+  }
+
+  int is_starting_dinner = 0;
+  if (read(client_socket, &is_starting_dinner, sizeof(is_starting_dinner)) == -1) {
+    perror("Error on reading for start the dinner");
+    exit(1);
+  }
+  start_dinner(id, client_socket, viewer_socket);
+  // Закрытие клиентского сокета
+  close(client_socket);
+
+  return 0;
+}
